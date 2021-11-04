7ea0bcafec3489a861e03a6fd80c076730fab666
Adebare Amos <adeinfo2015@gmail.com>
2021-11-04 12:36:49 +0100

    removed registration

---
+++ src/components/maintainace/Maintain.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./maintain.css";
+
+export const Maintain = () => {
+  return (
+    <div className="contain">
+      <div>
+        <h1>Sorry!</h1>
+        <p>
+          This page is currently under maintenance. We will be back online soon
+          Kindly check back
+        </p>
+      </div>
+      <div className="picture_bg">
+        {/* <p>
+          <small>
+            Image <i class="far fa-copyright"></i> corklifebyhilary
+          </small>
+        </p> */}
+      </div>
+    </div>
+  );
+};
